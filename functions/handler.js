cdf9997c3da4ac4f723e2ecad88af749ef0af199
Gregory Kysenko <heyapple1395@gmail.com>
2018-07-27 15:14:10 +0300

    dev

--- functions/handler.js
+++ functions/handler.js
@@ -9,7 +9,7 @@ module.exports.test = (event, context, callback) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'success'
+            message: 'You are successfully authorized'
         })
     }
 
